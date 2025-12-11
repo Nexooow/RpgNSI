e7d4a67001b4a14494a5a8f7fd4e1ac5609db902
Nexooow <contact.nekocaffe@gmail.com>
2025-12-11 14:23:37 +0100

    refactorisation du menu accueil

--- menu/accueil.py
+++ menu/accueil.py
@@ -1,15 +1,15 @@
-import pygame
-import uuid
-import random
-import os
 import json
+import os
+import random
+import uuid
+
+import pygame
 
 from lib.render import text_render_centered
 from menu.Menu import Menu
 
 
-class Accueil (Menu):
-
+class Accueil(Menu):
     def __init__(self, jeu):
         self.jeu = jeu
         try:
@@ -25,101 +25,133 @@ class Accueil (Menu):
         self.ouvrir()
 
     def ouvrir(self):
-        pygame.mixer.music.load('./assets/music/intro.mp3')
+        pygame.mixer.music.load("./assets/music/intro.mp3")
         pygame.mixer.music.set_volume(0.01)
-        pygame.mixer.music.play(1)
+        pygame.mixer.music.play(-1)
+        self.fade = 255
 
     def fermer(self):
         pygame.mixer.music.stop()
-
-    def update(self, events):
+        
+    def update_page_main (self, events):
         for event in events:
-            if self.sous_page == "main":
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-                    if self.menu_selected_option != 0:
-                        self.menu_selected_option -= 1
-                    if self.menu_selected_option == 1 and len(self.saves) == 0:
-                        self.menu_selected_option = 0
-                elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-                    if self.menu_selected_option != 3:
-                        self.menu_selected_option += 1
-                    if self.menu_selected_option == 1 and len(self.saves) == 0:
-                        self.menu_selected_option = 2
-                elif event.type == pygame.KEYDOWN and (event.key == pygame.K_SPACE):
-                    self.fermer()
-                    if self.menu_selected_option == 0:
-                        self.jeu.fade = 500
-                        sound = pygame.mixer.Sound(
-                            "./assets/sounds/accueil_clique.mp3")
-                        sound.set_volume(0.25)
-                        sound.play()
-                        self.jeu.demarrer(str(uuid.uuid4()))
-                    elif self.menu_selected_option == 1 and len(self.saves) > 0:
-                        self.menu_selected_option = 0
-                        self.jeu.fade = 200
-                        self.sous_page = "sauvegardes"
-                        self.jeu.fade = 150
-                    elif self.menu_selected_option == 2:
-                        pass
-                    elif self.menu_selected_option == 3:
-                        self.jeu.quitter()
-            elif self.sous_page == "sauvegardes":
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+                if self.menu_selected_option != 0:
                     self.menu_selected_option -= 1
-                    if self.menu_selected_option < 0:
-                        self.menu_selected_option = len(self.saves)-1
-                elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+                if self.menu_selected_option == 1 and len(self.saves) == 0:
+                    self.menu_selected_option = 0
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+                if self.menu_selected_option != 3:
                     self.menu_selected_option += 1
-                    if self.menu_selected_option == len(self.saves):
-                        self.menu_selected_option = 0
-                elif event.type == pygame.KEYDOWN and (event.key == pygame.K_SPACE):
-                    self.fermer()
+                if self.menu_selected_option == 1 and len(self.saves) == 0:
+                    self.menu_selected_option = 2
+            elif event.type == pygame.KEYDOWN and (event.key == pygame.K_SPACE):
+                self.fermer()
+                if self.menu_selected_option == 0:
                     self.jeu.fade = 500
-                    sound = pygame.mixer.Sound(
-                        "./assets/sounds/accueil_clique.mp3")
+                    sound = pygame.mixer.Sound("./assets/sounds/accueil_clique.mp3")
                     sound.set_volume(0.25)
                     sound.play()
-                    partie_choisie = self.saves[self.menu_selected_option]
-                    assert isinstance(partie_choisie, dict)
-                    self.jeu.demarrer(partie_choisie["id"], partie_choisie)
+                    self.jeu.demarrer(str(uuid.uuid4()))
+                elif self.menu_selected_option == 1 and len(self.saves) > 0:
+                    self.menu_selected_option = 0
+                    self.jeu.fade = 200
+                    self.sous_page = "sauvegardes"
+                    self.jeu.fade = 150
+                elif self.menu_selected_option == 2:
+                    pass
+                elif self.menu_selected_option == 3:
+                    self.jeu.quitter()
+        
+    def update_page_sauvegardes (self, events):
+        for event in events:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+                self.menu_selected_option -= 1
+                if self.menu_selected_option < 0:
+                    self.menu_selected_option = len(self.saves) - 1
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+                self.menu_selected_option += 1
+                if self.menu_selected_option == len(self.saves):
+                    self.menu_selected_option = 0
+            elif event.type == pygame.KEYDOWN and (event.key == pygame.K_SPACE):
+                self.fermer()
+                self.jeu.fade = 1000
+                sound = pygame.mixer.Sound("./assets/sounds/accueil_clique.mp3")
+                sound.set_volume(0.25)
+                sound.play()
+                partie_choisie = self.saves[self.menu_selected_option]
+                assert isinstance(partie_choisie, dict)
+                self.jeu.demarrer(partie_choisie["id"], partie_choisie)
+
+    def update(self, events):
+        if self.sous_page == "main":
+            self.update_page_main(events)
+        elif self.sous_page == "sauvegardes":
+            self.update_page_sauvegardes(events)
 
     def draw_main(self):
+        text_render_centered(
+            self.jeu.fond,
+            "Game Name",
+            "extrabold",
+            color=(255, 255, 255),
+            pos=(1000 / 2, 175),
+            size=128,
+        )
 
-        text_render_centered(self.jeu.fond, "Game Name", "extrabold", color=(
-            255, 255, 255), pos=(1000/2, 175), size=128)
-
-        text_render_centered(self.jeu.fond, "Créer une partie", "regular",
-                             color=(245, 205, 0, 185),
-                             pos=(1000/2, 350),
-                             underline=self.menu_selected_option == 0
-                             )
-        text_render_centered(self.jeu.fond, "Charger une partie", "regular",
-                             color=(245, 205, 0, 185) if len(
-                                 self.saves) > 0 else (255-100, 215-100, 0, 140),
-                             pos=(1000/2, 400),
-                             underline=self.menu_selected_option == 1
-                             )
-        text_render_centered(self.jeu.fond, "Paramètres", "regular",
-                             color=(245, 205, 0, 185),
-                             pos=(1000/2, 450),
-                             underline=self.menu_selected_option == 2
-                             )
-        text_render_centered(self.jeu.fond, "Quitter", "regular",
-                             color=(245, 205, 0, 185),
-                             pos=(1000/2, 500),
-                             underline=self.menu_selected_option == 3
-                             )
+        text_render_centered(
+            self.jeu.fond,
+            "Créer une partie",
+            "regular",
+            color=(245, 205, 0, 185),
+            pos=(1000 / 2, 350),
+            underline=self.menu_selected_option == 0,
+        )
+        text_render_centered(
+            self.jeu.fond,
+            "Charger une partie",
+            "regular",
+            color=(245, 205, 0, 185)
+            if len(self.saves) > 0
+            else (255 - 100, 215 - 100, 0, 140),
+            pos=(1000 / 2, 400),
+            underline=self.menu_selected_option == 1,
+        )
+        text_render_centered(
+            self.jeu.fond,
+            "Paramètres",
+            "regular",
+            color=(245, 205, 0, 185),
+            pos=(1000 / 2, 450),
+            underline=self.menu_selected_option == 2,
+        )
+        text_render_centered(
+            self.jeu.fond,
+            "Quitter",
+            "regular",
+            color=(245, 205, 0, 185),
+            pos=(1000 / 2, 500),
+            underline=self.menu_selected_option == 3,
+        )
 
     def draw_sauvegardes(self):
-
-        text_render_centered(self.jeu.fond, "Sauvegardes", "bold", color=(
-            255, 255, 255), pos=(1000/2, 175), size=86)
+        text_render_centered(
+            self.jeu.fond,
+            "Sauvegardes",
+            "bold",
+            color=(255, 255, 255),
+            pos=(1000 / 2, 175),
+            size=86,
+        )
 
         for index, save in enumerate(self.saves):
-            text_render_centered(self.jeu.fond, f"{save['id']}", "regular",
+            text_render_centered(
+                self.jeu.fond,
+                f"{save['id']}",
+                "regular",
                 color=(245, 205, 0, 185),
-                pos = (1000/2, 175+25+50*(index+1)),
-                underline=self.menu_selected_option == index
+                pos=(1000 / 2, 175 + 25 + 50 * (index + 1)),
+                underline=self.menu_selected_option == index,
             )
 
     def draw(self):
@@ -128,12 +160,12 @@ class Accueil (Menu):
             particule_position = random.randint(0, 1000)
             particule_taille = random.randint(1, 2)
             alpha = random.randint(80, 225)
-            self.particules.append(
-                ([particule_position, 700], alpha, particule_taille))
+            self.particules.append(([particule_position, 700], alpha, particule_taille))
 
         for particle in self.particules:
-            pygame.draw.circle(self.jeu.fond, (245, 205, 0,
-                               particle[1]), particle[0], particle[2])
+            pygame.draw.circle(
+                self.jeu.fond, (245, 205, 0, particle[1]), particle[0], particle[2]
+            )
             particle[0][1] -= 0.25
             if particle[0][1] < 0:
                 self.particules.remove(particle)
